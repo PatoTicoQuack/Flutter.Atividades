f8786410cfa5843f83015020a2e29a664d91be15
[PatoTicoQuack] <mrogeriopesarini@gmail.com>
2024-03-25 00:17:33 -0300

    Progresso...

--- CursoFlutterDart/basico_flutter/aula28widget/widget/lib/main.dart
+++ CursoFlutterDart/basico_flutter/aula28widget/widget/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main () => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
+class _PerguntaAppState extends State<PerguntaApp> { // classe privada
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
     print('Pergunta respondida');
   }
 
@@ -22,24 +27,29 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[0]),
+            Questao(perguntas[_perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: () {
-                print('Resposta 2 foi selecionada');
-              },
+              onPressed: _responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: () => print('Resposta 3 foi selecionada'),
+              onPressed: _responder,
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
